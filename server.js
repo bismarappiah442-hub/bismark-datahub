bbe286a1c9f405d5e2d2be3ec459f3c00834d5d6
bismarappiah442-hub <bismarappiah442@gmail.com>
2026-04-03 00:15:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/api/health', (req, res) => res.json({ status: 'ok', app: 'DataHub GH' 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT || 5000, () => {
+    app.listen(process.env.PORT || 10000, () => {
       console.log(`Server running on port ${process.env.PORT || 5000}`);
     });
   })
